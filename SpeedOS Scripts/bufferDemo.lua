3dbcfca4e9a01f922b8f98bb51e9afe2d133abf7
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-06-01 19:56:46 +0300

    Update bufferDemo.lua

--- SpeedOS Scripts/bufferDemo.lua
+++ SpeedOS Scripts/bufferDemo.lua
@@ -1,8 +1,18 @@
+--[[
+      Демонстрация работы библиотеки buffering, которая уменьшает
+      общее количество обращений к библиотеке term, записывая ее значения,
+      и изменяя их только в том случае, если заданные пользователем
+      значения отличаются от записанных. Короче говоря - буфферизация!
+]]
+
 SpeedOS.LoadAPI("SpeedAPI/buffering")
+
 buffering.setBackgroundColor(colors.white)
 term.clear()
+
 buffering.setTextColor(colors.black)
 buffering.setCursorPos(1, 1)
+
 term.write("Hello, World!")
 buffering.setCursorPos(1, 2)
 print("Background Color: "..buffering.backgroundColor)
