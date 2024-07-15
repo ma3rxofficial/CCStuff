f62342405f097cd6579283ab2a5b704a22d15b40
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-07-16 00:28:25 +0300

    Update Pastebin.lua

--- Installer/Pastebin.lua
+++ Installer/Pastebin.lua
@@ -1,3 +1,5 @@
+-----------------------------------------------------------------------------------------------------------------------------------
+
 function FancyError()
   term.setBackgroundColor(colors.black)
   term.clear()
@@ -5,14 +7,20 @@ function FancyError()
   print("Can't connect to github.com")
 end
 
+-----------------------------------------------------------------------------------------------------------------------------------
+
 response = http.get("https://raw.githubusercontent.com/ma3rxofficial/CCStuff/main/Installer/Installer.lua")
 
 if not response then
 	FancyError()
 end
 
+-----------------------------------------------------------------------------------------------------------------------------------
+
 file = fs.open("Installer.lua", "w")
 file.write(response.readAll())
 file.close()
 
 shell.run("Installer.lua")
+
+-----------------------------------------------------------------------------------------------------------------------------------
